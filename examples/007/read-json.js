6c902805c7c36ff78c02cf19a5342af214776e43
Michael_Claus <mclausaudio@gmail.com>
2020-02-10 20:09:31 -0800

    007

--- examples/007/read-json.js
+++ examples/007/read-json.js
@@ -1,5 +1,11 @@
-const { projectId, startDate, endDate } = require('./data/example-config');
+const { projectId, startDate, endDate } = require("./data/example-config");
 
 console.log(`The ID of the project is: ${projectId}`);
 console.log(`The start date of the project is: ${startDate}`);
 console.log(`The end date of the project is: ${endDate}`);
+
+// Michael's code
+
+const json = require("./data/example-config");
+
+console.log(json);
